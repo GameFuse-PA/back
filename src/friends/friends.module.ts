cd6a448c84964b71b67d01cfac22964f71174221
Emmanuel <manuboisset@gmail.com>
2023-04-27 00:13:49 +0200

    Fix module import

--- src/friends/friends.module.ts
+++ src/friends/friends.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FriendsController } from './friends.controller';
 import { FriendsService } from './friends.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [UsersModule],
+    imports: [UsersModule, JwtModule],
     controllers: [FriendsController],
     providers: [FriendsService],
 })
--- src/profil/profil.module.ts
+++ src/profil/profil.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProfilController } from './profile.controller';
 import { UsersModule } from '../users/users.module';
 import { ProfilService } from './profile.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [UsersModule],
+    imports: [UsersModule, JwtModule],
     controllers: [ProfilController],
     providers: [ProfilService],
 })
