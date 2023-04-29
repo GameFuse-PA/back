bdb31de25ec658933f81f2dcfebf837594cef796
Noe MARLEIX <noe@marleix.com>
2023-04-29 23:13:22 +0200

    text chat is working, not video

---
+++ src/liveChat/liveChat.controller.ts
@@ -0,0 +1,41 @@
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+    cors: { origin: '*', methods: ['GET', 'POST'] },
+    path: '/socket',
+})
+export class LiveChatController
+    implements OnGatewayConnection, OnGatewayDisconnect
+{
+    @WebSocketServer()
+    server: Server;
+    handleConnection(client: Socket) {
+        console.log('Client connected:', client.id);
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log('Client disconnected:', client.id);
+    }
+
+    @SubscribeMessage('join-room')
+    handleJoinRoom(client: Socket, payload: any) {
+        const { roomId, userId } = payload;
+        client.join(roomId);
+        client.broadcast.to(roomId).emit('user-connected', userId);
+
+        client.on('disconnect', () => {
+            client.broadcast.to(roomId).emit('user-disconnected', userId);
+        });
+
+        client.on('chat', (content) => {
+            client.broadcast.to(roomId).emit('new-message', content);
+        });
+    }
+}
---
+++ src/liveChat/liveChat.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { LiveChatController } from './liveChat.controller';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [LiveChatController],
+})
+export class LiveChatModule {}
