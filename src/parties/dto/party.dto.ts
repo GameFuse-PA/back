4707f67d08905f5f9790aee91074cbc39d0d09d2
Emmanuel Boisset <manuboisset@gmail.com>
2023-06-19 19:53:06 +0200

    Update dto Party

--- src/parties/dto/party.dto.ts
+++ src/parties/dto/party.dto.ts
@@ -1,10 +1,18 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class PartyDto {
+    @IsNotEmpty({ message: 'Nom requis' })
+    name: string;
+
+    @IsOptional()
+    description: string;
+
     @IsOptional()
     createdBy: string;
 
-    @IsNotEmpty({ message: 'Nom requis' })
+    @IsNotEmpty({
+        message: 'Au moins un membre est requis pour créer la partie',
+    })
     members: string[];
 
     @IsNotEmpty({ message: 'Un jeu est requis' })
