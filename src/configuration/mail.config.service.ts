dd23e8f393426287d0f0f76f503e98ba92cb9863
Emmanuel <manuboisset@gmail.com>
2023-06-28 20:51:22 +0200

    variabilisation url front

--- src/configuration/mail.config.service.ts
+++ src/configuration/mail.config.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as mustache from 'mustache';
+import { AppConfigService } from './app.config.service';
 
 @Injectable()
 export class MailConfigService {
     private readonly mailTemplates: any;
 
-    constructor() {
+    constructor(private readonly appConfiguration: AppConfigService) {
         const mailData = fs.readFileSync('src/utils/mails.json', 'utf8');
         this.mailTemplates = JSON.parse(mailData);
     }
@@ -21,9 +22,10 @@ export class MailConfigService {
 
     getInvitationMail(username: string, token: string) {
         const mail = this.mailTemplates.invitations;
+        const urlFront = this.appConfiguration.frontUrl;
         return {
             subject: mail.subject,
-            body: this.renderMailBody(mail.html, { username, token }),
+            body: this.renderMailBody(mail.html, { username, token, urlFront }),
         };
     }
 
