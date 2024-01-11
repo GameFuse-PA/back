a5169e4ece2c095f6a5fa599366ceb9fd09b5b1d
Pierre <pbonnamy.pro@gmail.com>
2024-01-11 19:17:06 +0100

    lint

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,29 +1,29 @@
 terraform {
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
       version = "2.34.0"
     }
 
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.4.1"
     }
 
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.24.0"
     }
   }
 
   backend "s3" {
-    bucket = "gamefuse-terraform-state"
-    key    = "gamefuse-terraform.tfstate"
-    endpoint = "https://s3.fr-par.scw.cloud"
-    region = "fr-par"
+    bucket                      = "gamefuse-terraform-state"
+    key                         = "gamefuse-terraform.tfstate"
+    endpoint                    = "https://s3.fr-par.scw.cloud"
+    region                      = "fr-par"
     skip_credentials_validation = true
-    skip_region_validation = true
-    skip_requesting_account_id = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
   }
 }
 
@@ -38,8 +38,8 @@ module "kubernetes_cluster" {
 }
 
 provider "kubernetes" {
-  host  = module.kubernetes_cluster.kubeconfig_host
-  token = module.kubernetes_cluster.kubeconfig_token
+  host                   = module.kubernetes_cluster.kubeconfig_host
+  token                  = module.kubernetes_cluster.kubeconfig_token
   cluster_ca_certificate = base64decode(
     module.kubernetes_cluster.kubeconfig_cluster_ca_certificate,
   )
@@ -50,15 +50,15 @@ module "database_deployment" {
 }
 
 module "api_deployment" {
-  source = "./api_deployment"
-  SCW_ACCESS_KEY = var.SCW_ACCESS_KEY
-  SCW_SECRET_KEY = var.SCW_SECRET_KEY
-  BUCKET_NAME = module.object_storage.bucket_name
-  BUCKET_REGION = module.object_storage.bucket_region
-  REGISTRY_USERNAME = var.REGISTRY_USERNAME
-  MAILJET_API_KEY = var.MAILJET_API_KEY
+  source             = "./api_deployment"
+  SCW_ACCESS_KEY     = var.SCW_ACCESS_KEY
+  SCW_SECRET_KEY     = var.SCW_SECRET_KEY
+  BUCKET_NAME        = module.object_storage.bucket_name
+  BUCKET_REGION      = module.object_storage.bucket_region
+  REGISTRY_USERNAME  = var.REGISTRY_USERNAME
+  MAILJET_API_KEY    = var.MAILJET_API_KEY
   MAILJET_API_SECRET = var.MAILJET_API_SECRET
   MAILJET_EMAIL_FROM = var.MAILJET_EMAIL_FROM
-  JWT_SECRET = var.JWT_SECRET
-  JWT_EXPIRATION = var.JWT_EXPIRATION
+  JWT_SECRET         = var.JWT_SECRET
+  JWT_EXPIRATION     = var.JWT_EXPIRATION
 }
--- terraform/main/object_storage/main.tf
+++ terraform/main/object_storage/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
       version = "2.34.0"
     }
   }
 }
 
 resource "scaleway_object_bucket" "gamefuse-bucket" {
-  name = "gamefuse-bucket"
+  name          = "gamefuse-bucket"
   force_destroy = true
-  region = "fr-par"
+  region        = "fr-par"
 }
