53b50c79ab2380cf81d3ccf91557d355c42fc5e5
Pierre <pbonnamy.pro@gmail.com>
2024-01-11 16:12:45 +0100

    clean variable

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -49,46 +49,6 @@ module "database_deployment" {
   source = "./database_deployment"
 }
 
-variable "SCW_ACCESS_KEY" {
-  type = string
-}
-
-variable "SCW_SECRET_KEY" {
-  type = string
-}
-
-variable "DOCKERHUB_USERNAME" {
-  type = string
-}
-
-variable "DOCKERHUB_PASSWORD" {
-  type = string
-}
-
-variable "DOCKERHUB_EMAIL" {
-  type = string
-}
-
-variable "MAILJET_API_KEY" {
-  type = string
-}
-
-variable "MAILJET_API_SECRET" {
-  type = string
-}
-
-variable "MAILJET_EMAIL_FROM" {
-  type = string
-}
-
-variable "JWT_SECRET" {
-  type = string
-}
-
-variable "JWT_EXPIRATION" {
-  type = string
-}
-
 module "api_deployment" {
   source = "./api_deployment"
   SCW_ACCESS_KEY = var.SCW_ACCESS_KEY
---
+++ terraform/main/variable.tf
@@ -0,0 +1,39 @@
+variable "SCW_ACCESS_KEY" {
+  type = string
+}
+
+variable "SCW_SECRET_KEY" {
+  type = string
+}
+
+variable "DOCKERHUB_USERNAME" {
+  type = string
+}
+
+variable "DOCKERHUB_PASSWORD" {
+  type = string
+}
+
+variable "DOCKERHUB_EMAIL" {
+  type = string
+}
+
+variable "MAILJET_API_KEY" {
+  type = string
+}
+
+variable "MAILJET_API_SECRET" {
+  type = string
+}
+
+variable "MAILJET_EMAIL_FROM" {
+  type = string
+}
+
+variable "JWT_SECRET" {
+  type = string
+}
+
+variable "JWT_EXPIRATION" {
+  type = string
+}
